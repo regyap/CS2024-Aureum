f021837dc9f77ebccd962bc63be767f545b6b827
Bing Hong <binghong.lim@gmail.com>
2023-10-26 15:34:28 +0800

    Changes to file structure

---
+++ Auruem/src/pages/Page1.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function Page1() {
+  return (
+    <>
+      <div>Page1</div>
+    </>
+  );
+}
+
+export default Page1;
---
+++ Auruem/src/pages/Page2.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function Page2() {
+  return (
+    <>
+      <div>Page2</div>
+    </>
+  );
+}
+
+export default Page2;
