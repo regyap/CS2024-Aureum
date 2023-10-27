c19599e4351b64e60298ead2e1b06155bc976276
ZhaoYiiii <66521076+ZhaoYiiii@users.noreply.github.com>
2023-10-27 16:06:28 +0800

    footer component, font folder
    not sure why font looks different

--- Auruem/src/App.jsx
+++ Auruem/src/App.jsx
@@ -3,10 +3,11 @@ import "./App.css";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Page1 from "./pages/Page1";
 import Page2 from "./pages/Page2";
+import Footer from "./components/Footer/Footer";
 
 function App() {
 
-  const count, setCount = useState(1);
+  const [count, setCount] = useState(1);
   
   return (
     <>
@@ -15,6 +16,7 @@ function App() {
           {/* <Route path="/" Component={WelcomePage}></Route> */}
           <Route path="/Page1" Component={Page1}></Route>
           <Route path="/Page2" Component={Page2}></Route>
+          <Route path="/Footer" Component={Footer}></Route>
         </Routes>
       </BrowserRouter>
     </>
