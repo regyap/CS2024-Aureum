8d65507c14fea1ccfa929bf79973bcb67c350014
Bing Hong <binghong.lim@gmail.com>
2023-11-19 09:48:24 +0800

    layout cleaning

--- Aureum(HTML)/js/index.js
+++ Aureum(HTML)/js/index.js
@@ -17,23 +17,4 @@ document.addEventListener("DOMContentLoaded", function () {
   setInterval(nextSlide, 3000);
   // Initial image load
   updateImage();
-
-  // Second Slideshow
-  const slideshow2 = [
-    "./image/slideshow-1.jpg",
-    "./image/slideshow-2.jpg",
-    "./image/slideshow-3.jpg",
-  ];
-  let j = 0;
-  function updateImage2() {
-    document.getElementById("slideshowImage2").src = slideshow2[j];
-  }
-  function nextSlide2() {
-    j = (j + 1) % slideshow2.length;
-    updateImage2();
-  }
-  // Set an interval to change the slide every 3000 milliseconds (3 seconds)
-  setInterval(nextSlide2, 3000);
-  // Initial image load
-  updateImage2();
 });
--- Aureum(HTML)/js/login.js
+++ Aureum(HTML)/js/login.js
@@ -1,3 +1,31 @@
+function formatDate(date) {
+  const options = { day: "numeric", month: "numeric", year: "numeric" };
+  const [month, day, year] = new Date(date)
+    .toLocaleDateString(undefined, options)
+    .split("/");
+  return `${day}/${month}/${year}`;
+}
+
+function showDateInput() {
+  document.getElementById("textInput").style.display = "none";
+  document.getElementById("dateInput").style.display = "inline-block";
+  // Set the formatted date value of dateInput to the formatted value of textInput
+  document.getElementById("dateInput").value = formatDate(
+    document.getElementById("textInput").value
+  );
+}
+
+function showTextInput() {
+  document.getElementById("dateInput").style.display = "none";
+  document.getElementById("textInput").style.display = "inline-block";
+  // Set the formatted value of textInput to the formatted date value of dateInput
+  document.getElementById("textInput").value = document.getElementById(
+    "dateInput"
+  ).value
+    ? formatDate(document.getElementById("dateInput").value)
+    : document.getElementById("textInput").value;
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   const loginForm = document.getElementById("login-section");
   const registerForm = document.getElementById("register-section");
@@ -19,4 +47,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Initial state
   showLoginForm();
+  document.getElementById("dateInput").style.display = "none";
+  document.getElementById("textInput").style.display = "inline-block";
 });
